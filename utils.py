c5fb46103e2d1e55bbb1cbef2275d6208a0f5de9
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2024-09-11 18:55:43 -0500

    added prompt()

--- utils.py
+++ utils.py
@@ -6,3 +6,28 @@ def msg(message):
 
 def erm(message):
     print(f" \x1b[31;1m*\x1b[39m {message}\x1b[0m")
+
+
+def prompt(message, default='n'):
+    match default:
+        case 'n':
+            prompt_message = f" - {message} (y/N)\n > "
+            ret = False
+        case 'y':
+            prompt_message = f" - {message} (Y/n)\n > "
+            ret = True
+        case _:
+            erm(f"Invalid default '{default}' for prompt()")
+            exit()
+
+    while True:
+        a = input(prompt_message).strip().lower()
+        match a:
+            case 'y' | 'yes':
+                return True
+            case 'n' | 'no':
+                return False
+            case '':
+                return ret
+            case _:
+                erm(f"Invalid input '{a}'")
