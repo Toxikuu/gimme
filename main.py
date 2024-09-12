7d5044cf9426e9582164ab497068f1b347f5716b
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2024-09-11 22:25:33 -0500

    multiple fixes and improvements

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # TODO: Improve error handling (especially for removing files, overwriting stuff in the src dir, etc)
 # TODO: Add a quiet option (can be done by adding a variable "> /dev/null" for commands)
 # TODO: Add more meta files
-from utils import erm, msg, prompt
+# TODO: Untrack older versions of updated packages (might be done with the purge action)
+# TODO: Improve package selection
+from utils import erm, msg, cmd, prompt, str_to_bool
 import subprocess
 import os
 import yaml
@@ -48,12 +50,15 @@ def interpolate(metafile):
 
 
 class PackageManager:
-    def __init__(self, sources_directory, meta_directory, tracking_file):
-        self.sources_directory = sources_directory
-        self.meta_directory = meta_directory
-        self.tracking_file = tracking_file
+    def __init__(self, cfg):
+        self.sources_directory = cfg["Directories"]["sources"]
+        self.meta_directory = cfg["Directories"]["meta"]
+        self.tracking_file = cfg["Files"]["tracking"]
 
-        with open(tracking_file, 'r') as f:
+        self.remove_build_dirs = str_to_bool(cfg["Removal"]["build_dirs"])
+        self.remove_tarballs = str_to_bool(cfg["Removal"]["tarballs"])
+
+        with open(self.tracking_file, 'r') as f:
             lines = f.readlines()
             lines = [line.strip() for line in lines if line.strip() != '']
         self.installed_packages = lines
@@ -64,76 +69,91 @@ class PackageManager:
         os.makedirs(os.path.join('..', self.meta_directory), exist_ok=True)
 
     def install_check(self, package):
+        msg(f"Checking if package '{package}' is installed...")
         if str(package) in self.installed_packages:
-            msg("Package installed")
             return prompt(f"Package '{package}' already installed! Reinstall?", default='n')
-        return False
+        return True
 
     def fetch_source(self, package):
         msg(f"Fetching source for {package}...")
         if os.path.isfile(package.tarball):
             msg(f"Tarball {package.tarball} already exists")
             return
-        subprocess.run(["wget", package.url, "-O", package.tarball], check=True)
+        if not cmd(f"wget '{package.url}' -O {package.tarball}"):
+            erm(f"Failed to wget tarball '{package.tarball}' from url '{package.url}'")
+            exit()
         msg(f"Source fetched and saved as {package.tarball}")
+
+    def extract_tarball(self, package):
+        msg(f"Extracting tarball '{package.tarball}'")
+
+        temp_dir = "temp_extract"
+        if os.path.isdir(temp_dir):
+            cmd(f"rm -rv {temp_dir}")
+        cmd("mkdir -v temp_extract")
+
+        if os.path.isdir(repr(package)):
+            cmd(f"rm -rvf {package}")
+
+        if not cmd(f"tar xvf {package.tarball} -C {temp_dir}"):
+            erm(f"Failed to extract tarball '{package.tarball}'")
+            msg(f"Removing possibly corrupted tarball '{package.tarball}'")
+            cmd(f"rm -v {package.tarball}")
+            msg("Try again")
+            exit()
+
+        cmd(f"mv -v {temp_dir}/* {package}")
+        cmd(f"rm -rv {temp_dir}")
 
     def build(self, package):
         msg(f"Building {package}...")
-        if package.tarball:
-            msg(f"Using tarball: {package.tarball}")
-
-            temp_dir = "temp_extract"
-            os.makedirs(temp_dir, exist_ok=True)
-
-            if os.path.isdir(repr(package)):
-                subprocess.run(f"rm -rvf ./{package}", shell=True, check=True)
-
-            subprocess.run(f"tar xvf {package.tarball} -C {temp_dir}", shell=True, check=True)
-            subprocess.run(f"mv -v temp_extract/* ./{package}", shell=True, check=True)
-            subprocess.run(f"rm -rv {temp_dir}", shell=True, check=True)
-
-            os.chdir(repr(package))
-            if package.get:
-                subprocess.run(package.get, shell=True, check=True)
-            os.chdir("..")
+        if cmd(f"cd {package} && {package.get}"):
+            msg(f"Successfully installed {package}")
         else:
-            erm(f"Tarball '{package.tarball}' not found.")
+            erm(f"Failed to install {package}")
+            exit()
 
-    def clean_up(self, package, and_dir=False):
-        if os.path.exists(f"{package}.tox"):
-            subprocess.run(["rm", "-vf", f"{package}.tox"])
-        if and_dir:
-            if os.path.exists(repr(package)):
-                subprocess.run(["rm", "-rv", repr(package)])
+    def clean_up(self, package, tarball=False, build_dir=False):
+        if tarball:
+            cmd(f"rm -vf {package.tarball}")
+        if build_dir:
+            cmd(f"rm -rvf {package}")
 
     def uninstall(self, package):
         msg(f"Uninstalling {package}...")
-        os.chdir(repr(package))
-        if package.remove:
-            subprocess.run(package.remove, shell=True, check=True)
-        os.chdir("..")
+        cmd(f"cd {package} && {package.remove}")
 
     def track_package(self, package, action):
+        def _write():
+            f.seek(0)
+            f.writelines(lines)
+            f.truncate()
+
         match action:
             case "remove":
+                msg(f"Untracking {package}...")
                 with open(os.path.join('..', self.tracking_file), 'r+') as f:
                     lines = f.readlines()
                     lines = [line for line in lines if repr(package) not in line]
-                    f.seek(0)
-                    f.writelines(lines)
-                    f.truncate()
+                    _write()
+
             case "add":
+                msg(f"Tracking {package}...")
                 with open(os.path.join('..', self.tracking_file), 'r+') as f:
-                    lines = f.read().splitlines()
+                    lines = f.readlines()
                     lines = list(dict.fromkeys(lines))
-                    newline = repr(package)
-                    if newline not in lines:
-                        lines.append(newline)
-                        f.seek(0)
-                        f.writelines('\n'.join(lines)+'\n')
-                        f.truncate()
+                    if f"{package}\n" not in lines:
+                        lines.append(f"{package}\n")
+                        _write()
                     else:
-                        msg(f"Package '{package}' already tracked.")
+                        msg(f"Package '{package}' already tracked")
+
+            case "purge":
+                msg(f"Purging {package}...")
+                with open(os.path.join('..', self.tracking_file), 'r+') as f:
+                    lines = f.readlines()
+                    lines = [line for line in lines if f"{package.name}-" not in line]
+                    _write()
 
     def list_installed(self):
         if not self.installed_packages:
@@ -144,14 +164,18 @@ class PackageManager:
                 print(f" - {p}")
 
     def get_package(self, package):
+        self.track_package(package, "purge")
         self.fetch_source(package)
+        self.extract_tarball(package)
         self.build(package)
-        self.clean_up(package)
+        self.clean_up(package,
+                      tarball=self.remove_tarballs,
+                      build_dir=self.remove_build_dirs)
         self.track_package(package, "add")
 
     def remove_package(self, package):
         self.uninstall(package)
-        self.clean_up(package, and_dir=True)
+        self.clean_up(package, tarball=True, build_dir=True)
         self.track_package(package, "remove")
 
 
@@ -177,6 +201,7 @@ class ControlPanel:
         parser.add_argument("-r", "--remove", type=str, help="remove <package>")
         parser.add_argument("-l", "--list", action="store_true", help="list installed packages")
         parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+        parser.add_argument("-q", "--quiet", action="store_true", help="decreases output verbosity")
 
         return parser.parse_args()
 
@@ -211,9 +236,7 @@ if __name__ == "__main__":
     cfg = read_config("gimme.conf")
 
     pm = PackageManager(
-        sources_directory=cfg["Directories"]["sources"],
-        meta_directory=cfg["Directories"]["meta"],
-        tracking_file=cfg["Files"]["tracking"]
+        cfg=cfg
         )
 
     pm.make_config_dirs()
