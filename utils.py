3602f363c37a06ec7d3c95e186380a887c8df37d
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2024-09-11 20:59:05 -0500

    added cmd()

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import subprocess
 
 
 def msg(message):
@@ -8,13 +9,22 @@ def erm(message):
     print(f" \x1b[31;1m*\x1b[39m {message}\x1b[0m")
 
 
+def cmd(command):
+    try:
+        subprocess.run(command, shell=True, check=True)
+        return True
+    except subprocess.CalledProcessError:
+        erm(f"Command '{command}' failed!")
+        return False
+
+
 def prompt(message, default='n'):
     match default:
         case 'n':
-            prompt_message = f" - {message} (y/N)\n > "
+            prompt_message = f" \x1b[33;3m*\x1b[39m {message} (y/N)\x1b[0m\n \x1b[33;1;3m>\x1b[0m "
             ret = False
         case 'y':
-            prompt_message = f" - {message} (Y/n)\n > "
+            prompt_message = f" \x1b[33;3m*\x1b[39m {message} (Y/n)\x1b[0m\n \x1b[33;1;3m>\x1b[0m "
             ret = True
         case _:
             erm(f"Invalid default '{default}' for prompt()")
