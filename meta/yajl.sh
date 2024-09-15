2205f9c4e5bcd250e4805d39485b67d085f49b42
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2024-09-15 16:34:17 -0500

    bugfixes

--- meta/yajl.sh
+++ meta/yajl.sh
@@ -9,15 +9,14 @@ get() {
   wget https://gitlab.archlinux.org/archlinux/packaging/packages/yajl/-/raw/main/yajl-$VERS-CVE-2022-24795.patch
   wget https://gitlab.archlinux.org/archlinux/packaging/packages/yajl/-/raw/main/yajl-$VERS-memory_leak.patch
 
-  patch -Np1 -d --verbose yajl-$VERS -i ./yajl-$VERS-CVE-2017-16516.patch
-  patch -Np1 -d --verbose yajl-$VERS -i ./yajl-$VERS-CVE-2022-24795.patch
-  patch -Np1 -d --verbose yajl-$VERS -i ./yajl-$VERS-memory_leak.patch
+  patch --verbose -Np1 -i ./yajl-$VERS-CVE-2017-16516.patch
+  patch --verbose -Np1 -i ./yajl-$VERS-CVE-2022-24795.patch
+  patch --verbose -Np1 -i ./yajl-$VERS-memory_leak.patch
 
   cmake                          \
     -B build                     \
     -D CMAKE_BUILD_TYPE=None     \
-    -D CMAKE_INSTALL_PREFIX=/usr \
-    -S yajl-$VERS                
+    -D CMAKE_INSTALL_PREFIX=/usr
   cmake --build build --verbose
   cmake --install build
 }
