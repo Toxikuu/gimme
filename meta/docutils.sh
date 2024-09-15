18c21f3c46c899c0a64f98e7fcb152fb9512852b
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2024-09-15 15:11:48 -0500

    testing

--- meta/docutils.sh
+++ meta/docutils.sh
@@ -5,9 +5,9 @@ DEPS="python"
 LINK="https://files.pythonhosted.org/packages/source/d/docutils/docutils-$VERS.tar.gz"
 
 get() {
-  for f in /usr/bin/rst*.py; do
-    rm -fv /usr/bin/$(basename $f .py)
-  done
+  # for f in /usr/bin/rst*.py; do
+  #   rm -fv /usr/bin/$(basename $f .py)
+  # done
 
   pip3 wheel -w dist --no-build-isolation --no-deps --no-cache-dir $PWD
   pip3 install --no-index --find-links=dist --no-cache-dir --no-user docutils
