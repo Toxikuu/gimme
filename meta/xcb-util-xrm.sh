4389bb6b8419c4c433b46273071098859756f261
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2024-09-15 16:34:34 -0500

    addition

---
+++ meta/xcb-util-xrm.sh
@@ -0,0 +1,41 @@
+NAME="xcb-util-xrm"
+VERS="1.3"
+TYPE="extra"
+DEPS="xcb-util"
+LINK="https://github.com/Airblader/xcb-util-xrm/archive/refs/tags/v$VERS.tar.gz"
+
+get() {
+  ./configure --prefix=/usr
+  make
+  make install
+}
+
+remove() {
+  echo not implemented
+}
+
+
+
+vars() {
+echo "$NAME"
+echo "$VERS"
+echo "$TYPE"
+echo "$DEPS"
+echo "$LINK"
+}
+
+case $1 in 
+  get)
+    get
+    ;;
+  remove)
+    remove
+    ;;
+  vars)
+    vars
+    ;;
+  *)
+    echo "INVALID \$1: $1"
+    exit 1
+    ;;
+esac
