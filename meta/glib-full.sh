a590315a5a4454e47eec7480183e858758c599d0
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2024-09-15 15:31:17 -0500

    testing

--- meta/glib-full.sh
+++ meta/glib-full.sh
@@ -22,10 +22,10 @@ get() {
   ninja &&
   ninja install &&
 
-  pushd ..
+  pushd ../..
   wget https://download.gnome.org/sources/gobject-introspection/1.80/gobject-introspection-1.80.1.tar.xz
   popd
-  tar xvf ../gobject-introspection-1.80.1.tar.xz &&
+  tar xvf ../../gobject-introspection-1.80.1.tar.xz &&
   meson setup gobject-introspection-1.80.1 gi-build \
             -D cairo=disabled -D doctool=disabled   \
             --prefix=/usr --buildtype=release     &&
